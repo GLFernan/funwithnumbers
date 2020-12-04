82a4e18c6698a7c882b141eca651da5587ce39d3
GLFernan <75393139+GLFernan@users.noreply.github.com>
2020-12-04 01:30:39 -0500

    Update gr2.py

--- views/gr2.py
+++ views/gr2.py
@@ -3,17 +3,20 @@ import math
 
 @error(500)
 def error500(error):
-    return '<h1 style="color:red;">Something besides an integer was taken as an input. Please go back and enter a positive integer only.'
+    return '<h1 style="color:red;">Something besides an integer was taken as an input after "/funwithnumbers/" Please go back and enter a positive integer only.' + \
+           '<h3> For example, input the number <a href ="http://localhost:8000/funwithnumbers/15">15</a> after the second forward slash.'
 
 @error(404)
 def error404(error):
-    return '<h1>Please make sure to type "/funwithnumbers/" properly after the 8000 and before following it up with a positive integer as the input.' +
-# example with hyperlink here
+    return '<h1>Please make sure to type "/funwithnumbers/" properly after the 8000 and before following it up with a positive integer as the input.' + \
+           '<h3>For example, you can type <a href ="http://localhost:8000/funwithnumbers/100">/funwithnumbers/100</a> in the searchbar.'
+
 @route('/')
 def index():
   return 'Append "/funwithnumbers/&lt;num&gt;" to the URL to see some cool things we will do with your number!</br>' + \
-         'Example:   /funwithnumbers/100  </br>' + \
-         ' IMPORTANT: Please input positive integers only!'
+         'Example:   /funwithnumbers/100  </br>'\
+         ' IMPORTANT: Please input positive integers only!'\
+         '<p>Also, check out our progress page <a href ="http://localhost:8000/funwithnumbers/0" target="_blank">here</a></p>'
 
 @route('/funwithnumbers/<num>')
 @view('gr2')
